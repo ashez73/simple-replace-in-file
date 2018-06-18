c65626f2b1b0c5b6a0614eae84e054121d803f25
Pawel <pawelp73@gmail.com>
2018-06-18 10:54:43 +0200

    async/await usage added

--- js/changeme_async.js
+++ js/changeme_async.js
@@ -1,49 +1,62 @@
 const readline = require('readline');
 const fs = require('fs');
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 RegExp.escape = string => string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 let answer = {};
 let strings = {};
 
-//get user input
+async function simpleReplace() {
 
-const userInput = new Promise(resolve => {
-    rl.question('What phrase should I look for? ', args => {
+  //get user input
+
+  function userInput() {
+    return new Promise(resolve => {
+      rl.question('What phrase should I look for? ', args => {
         answer.one = new RegExp(RegExp.escape(args), "g");
         rl.question('Phrase to replace with? : ', args => {
-            answer.two = args;
-            rl.close();
-            resolve(answer);
+          answer.two = args;
+          rl.close();
+          resolve(answer);
         });
+      });
     });
-});
-userInput.then(answer => {
-    //TODO: change to sync version for sake of consistency 
-    let fileArray = (fs.readdirSync('../files'));
-    let newString;
-    let myString;
-    console.log(`parsing following files: ${fileArray.toString()}`);
+  }
 
-    //main loop: read data/replace data/write data
-
-    for (let file of fileArray) {
-        let myFile = `../files/${file}`;
-        myString = new Promise(resolve => {
-            fs.readFile(myFile, 'utf-8', (err, data) => {
-                if (err) throw err;
-                strings.old =data;
-                strings.new = data.replace(answer.one, answer.two);
-                resolve(strings);
-            });
+  function readDir() {
+    return new Promise(resolve => {
+      fs.readdir('../files', (err, list) => {
+        if (err) { throw err };
+        //console.log(list)
+        resolve(list);
+      });
+    });
+  };
+  await userInput();
+  let list = await readDir();
+  async function updateFiles() {
+    for (let file of list) {
+      let myFile = `../files/${file}`;
+      myString = new Promise(resolve => {
+        fs.readFile(myFile, 'utf-8', (err, data) => {
+          if (err) throw err;
+          strings.old = data;
+          strings.new = data.replace(answer.one, answer.two);
+          resolve(strings);
+        });
+      })
+      strings = await myString;
+      if (strings.old != strings.new) {
+        fs.writeFile(myFile, strings.new, (err) => {
+          if (err) throw err;
+          console.log(`${myFile} was updated!`);
         })
-        myString.then(strings => {
-            if (strings.old != strings.new) {
-            fs.writeFile(myFile, strings.new, (err) => {
-                if (err) throw err;
-                console.log(`${myFile} was updated!`);
-            })}});
+      }
     }
-});
+  }
+  updateFiles();
+
+}
+simpleReplace();
